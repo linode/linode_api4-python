574355c9f88a1cd1314a464ff95c0fdff7af99bc
Will Smith <wsmith@linode.com>
2015-12-24 07:17:44 -0500

    Duplicate disks, reset root password

--- linode/objects/disk.py
+++ linode/objects/disk.py
@@ -16,3 +16,37 @@ class Disk(DerivedBase):
         'updated': Property(is_datetime=True),
         'linode_id': Property(identifier=True),
     }
+
+
+    def duplicate(self):
+        result = self._client.post(Disk.api_endpoint, model=self, data={})
+
+        if not 'disk' in result:
+            return result
+
+        d = Disk(self._client, result['disk']['id'], self.id)
+        d._populate(result['disk'])
+        return d
+
+
+    def reset_root_password(self, root_password=None):
+        rpass = root_password
+        if not rpass:
+            from .linode import Linode
+            rpass = Linode.generate_root_password()
+
+        params = {
+            'password': rpass,
+        }
+
+        result = self._client.post(Disk.api_endpoint, model=self, data=params)
+
+        if not 'disk' in result:
+            if not root_password:
+                return result, rpass
+            return result
+
+        self._populate(result['disk'])
+        if not root_password:
+            return True, rpass
+        return True
