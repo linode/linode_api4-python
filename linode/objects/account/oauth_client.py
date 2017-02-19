fa9edda401ed93e423a43dc1bd528faa9cfc7777
Dorthu <dorthu2@gmail.com>
2017-02-19 08:31:32 -0500

    Get OAuthClient thumbnail (and dump to file)

--- linode/objects/account/oauth_client.py
+++ linode/objects/account/oauth_client.py
@@ -1,3 +1,5 @@
+import requests
+
 from linode.objects import Base, Property
 
 class OAuthClient(Base):
@@ -24,9 +26,28 @@ class OAuthClient(Base):
         self._populate(result)
         return self.secret
 
-    def get_thumbnail(self):
-        # TODO
-        pass
+    def get_thumbnail(self, dump_to=None):
+        """
+        This returns binary data that represents a 128x128 image.
+        If dump_to is given, attempts to write the image to a file
+        at the given location.
+        """
+        headers = {
+            "Authorization": "token {}".format(self._client.token)
+        }
+
+        result = requests.get('{}/{}/thumbnail'.format(self._client.base_url,
+                OAuthClient.api_endpoint.format(id=self.id)),
+                headers=headers)
+
+        if not result.status_code == 200:
+            return False # TODO - handle this better?
+
+        if dump_to:
+            with open(dump_to, 'wb+') as f: # TODO -python2?
+                f.write(result.content)
+        else:
+            return result.content
 
     def set_thumbnail(self, thumb):
         # TODO
