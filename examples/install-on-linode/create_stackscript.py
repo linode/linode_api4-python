470693beb090f97d7c769d7ea0da9cbbcea36de9
Will Smith <wsmith@linode.com>
2017-08-28 10:37:55 -0400

    Fixed create_stackscript demo script

--- examples/install-on-linode/create_stackscript.py
+++ examples/install-on-linode/create_stackscript.py
@@ -4,7 +4,7 @@ from linode import LinodeClient
 import config
 
 token = input("Please provide an OAuth Token: ")
-client = LinodeClient(token, base_url=config.api_base_url)
+client = LinodeClient(token)
 s = client.linode.create_stackscript('Demonstration_Public', '#!/bin/bash',
         client.linode.get_distributions(), is_public=True)
 print("StackScript created, use this ID: {}".format(s.id))
