cb9276787cadcabe8d3939e3c8b54b3c57e5c123
Will Smith <wsmith@linode.com>
2017-10-25 20:43:58 -0400

    Adds the ability to test POST methods
    Adds the ability to mock request methods that aren't GET, specifically
    POST but others should be easy, and tests it with a test for
    create_linode

--- test/base.py
+++ test/base.py
@@ -15,16 +15,99 @@ class MockResponse:
     def json(self):
         return self._json
 
-def mock_get(url, headers=None, data=None):
+
+def load_json(url):
     """
-    Loads the response from a JSON file
+    Returns a dict from a .json file living in /test/response/GET/{url}.json
+
+    :param url: The URL being accessed whose JSON is to be returned
+
+    :returns: A dict containing the loaded JSON from that file
     """
-    file_path = sys.path[0] + '/responses/GET/' + url + '.json'
+    formatted_url = url
+
+    while formatted_url.startswith('/'):
+        formatted_url = formatted_url[1:]
+
+    formatted_url = formatted_url.replace('/', '_')
+
+    file_path = sys.path[0] + '/responses/GET/' + formatted_url + '.json'
 
     with open(file_path) as f:
         response = f.read()
 
-    return MockResponse(200, json.loads(response))
+    return json.loads(response)
+
+
+def mock_get(url, headers=None, data=None):
+    """
+    Loads the response from a JSON file
+    """
+    response = load_json(url)
+
+    return MockResponse(200, response)
+
+
+class MethodMock:
+    """
+    This class is used to mock methods on requests and store the parameters
+    and headers it was called with.
+    """
+    def __init__(self, method, return_dct):
+        """
+        Creates and initiates a new MethodMock with the given details
+
+        :param method: The HTTP method we are mocking
+        :param return_dct: The python dct to returned, or the URL for a JSON
+            file to return
+        """
+        self.method = method
+        if isinstance(return_dct, dict):
+            self.return_dct = return_dct
+        elif isinstance(return_dct, str):
+            self.return_dct = load_json(return_dct)
+        else:
+            raise TypeError('return_dct must be a dict or a URL from which the '
+                            'JSON could be loaded')
+
+    def __enter__(self):
+        """
+        Begins the method mocking
+        """
+        self.mock = patch(
+            'linode.linode_client.requests.'+self.method,
+            return_value=MockResponse(200, self.return_dct)
+        )
+        self.mock.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Removed the mocked method
+        """
+        self.mock.stop()
+
+    @property
+    def call_args(self):
+        # TODO - these don't work :(
+        """
+        A shortcut to accessing the underlying mock object's call args
+        """
+        return self.mock.call_args
+
+    @property
+    def call_data(self):
+        """
+        A shortcut to getting the data param this was called with
+        """
+        return self.mock.call_args[1]['data']
+
+    @property
+    def call_headers(self):
+        """
+        A shortcut to getting the headers param this was called with
+        """
+        return self.mock.call_args[1]['headers']
 
 
 class ClientBaseCase(TestCase):
@@ -37,3 +120,14 @@ class ClientBaseCase(TestCase):
 
     def tearDown(self):
         self.get_patch.stop()
+
+    def mock_post(self, return_dct):
+        """
+        Returns a MethodMock mocking a POST.  This should be used in a with
+        statement.
+
+        :param return_dct: The JSON that should be returned from this POST
+
+        :returns
+        """
+        return MethodMock('post', return_dct)
--- test/linode_client_test.py
+++ test/linode_client_test.py
@@ -4,7 +4,7 @@ class LinodeClientGeneralTest(ClientBaseCase):
     """
     Tests methods of the LinodeClient class that do not live inside of a group.
     """
-    def test_GET_regions(self):
+    def test_get_regions(self):
         r = self.client.get_regions()
 
         self.assertEqual(len(r), 9)
@@ -12,3 +12,23 @@ class LinodeClientGeneralTest(ClientBaseCase):
             self.assertTrue(region._populated)
             self.assertIsNotNone(region.id)
             self.assertIsNotNone(region.country)
+
+
+class LinodeGroupTest(ClientBaseCase):
+    """
+    Tests methods of the LinodeGroup
+    """
+    def test_create_linode(self):
+        """
+        Tests that a Linode can be created successfully
+        """
+        with self.mock_post('linodes/123') as m:
+            l = self.client.linode.create_instance('us-east-1a', 'g5-standard-1')
+
+            self.assertIsNotNone(l)
+            self.assertEqual(l.id, 123)
+            #
+            #self.assertEqual(m.call_data, {
+            #    "region": "us-east-1a",
+            #    "type": "g5-standard-1"
+            #})
