3e67420732af7a5e902c9e1fb8c556d92862349b
Will Smith <wsmith@linode.com>
2017-11-21 20:40:02 -0500

    Update User and UserGrants for the User Grant change
    A recent api release changed the way grants worked for restricted users.
    This follows that change, allowing grants to be managed and retrieved.

--- linode/objects/account/user_grant.py
+++ linode/objects/account/user_grant.py
@@ -3,10 +3,7 @@ from linode.objects.dbase import DerivedBase
 from linode.objects.domain import Domain
 from linode.objects.linode import Linode, StackScript, Volume
 from linode.objects.nodebalancer import NodeBalancer
-
-normal_grants = ('all', 'access', 'delete')
-stackscript_grants = ('all', 'use', 'edit', 'delete')
-linode_grants = ('all', 'access', 'delete', 'resize')
+from linode.objects.longview import LongviewClient
 
 
 def get_obj_grants():
@@ -17,26 +14,37 @@ def get_obj_grants():
             ('domain', Domain),
             ('stackscript', StackScript),
             ('nodebalancer', NodeBalancer),
-            ('volumes', Volume))
+            ('volume', Volume),
+            #('image', Image),
+            ('longview', LongviewClient))
 
 
 class Grant:
+    """
+    A Grant is a single grant a user has to an object.  A Grant's entity is
+    an object on the account, such as a Linode, NodeBalancer, or Volume, and
+    its permissions level is one of None, "read_only" or "read_write".
+
+    Grants cannot be accessed or updated individually, and are only relevant in
+    the context of a UserGrants object.
+    """
     def __init__(self, client, cls, dct):
         self._client = client
         self.cls = cls
         self.id = dct['id']
         self.label = dct['label']
-        self.grants = normal_grants
-        if cls is Linode:
-            self.grants = linode_grants
-        elif cls is StackScript:
-            self.grants = stackscript_grants
-
-        for g in self.grants:
-            setattr(self, g, dct[g])
+        self.permissions = dct['permissions']
 
     @property
     def entity(self):
+        """
+        Returns the object this grant is for.  The objects type depends on the
+        type of object this grant is applied to, and the object returned is
+        not populated (accessing its attributes will trigger an api request).
+
+        :returns: This grant's entity
+        :rtype: Linode, NodeBalancer, Domain, StackScript, Volume, or Longview
+        """
         # there are no grants for derived types, so this shouldn't happen
         if not issubclass(self.cls, Base) or issubclass(self.cls, DerivedBase):
             raise ValueError("Cannot get entity for non-base-class {}".format(self.cls))
@@ -44,24 +52,37 @@ class Grant:
 
     def _serialize(self):
         """
-        Returns this grant in a PUT-able form
+        Returns this grant in as JSON the api will accept.  This is only relevant
+        in the context of UserGrants.save
         """
-        ret = {g: getattr(self, g) for g in self.grants}
-        ret['id'] = self.id
-        return ret
+        return {
+            'permissions': self.permissions,
+            'id': self.id
+        }
 
 
 class UserGrants:
+    """
+    The UserGrants object represents the grants given to a restricted user.
+    Each section of grants has a list of objects and the level of grants this
+    user has to that object.
+
+    This is not an instance of Base because it lacks most of the attributes of
+    a Base-like model (such as a unique, ID-based endpoint at which to access
+    it), however it has some similarities so that its usage is familiar.
+    """
     api_endpoint = "/account/users/{username}/grants"
     parent_id_name = 'username'
 
-    def __init__(self, client, username):
+    def __init__(self, client, username, json=None):
         self._client = client
         self.username = username
+
+        if json is not None:
+            self._populate(json)
     
     def _populate(self, json):
         self.global_grants = type('global_grants', (object,), json['global'])
-        self.customer = type('customer_grants', (object,), json['customer'])
 
         for key, cls in get_obj_grants():
             lst = []
@@ -72,7 +93,6 @@ class UserGrants:
     def save(self):
         req = {
             'global': {k: v for k, v in vars(self.global_grants).items() if not k.startswith('_')},
-            'customer': {k: v for k, v in vars(self.customer).items() if not k.startswith('_')},
         }
 
         for key, _ in get_obj_grants():
@@ -80,8 +100,9 @@ class UserGrants:
             for cg in getattr(self, key):
                 lst.append(cg._serialize())
             req[key] = lst
-        print(req)
 
         result = self._client.put(UserGrants.api_endpoint.format(username=self.username), data=req)
 
         self._populate(result)
+
+        return True
