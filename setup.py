6f296de0f7a2a6278cb3f87a1403486a4544fb69
Will Smith <wsmith@linode.com>
2017-12-12 13:51:05 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='4.1.4b4',
+    version='4.1.5b0',
 
     description='The official python SDK for Linode API v4',
     long_description=long_description,
