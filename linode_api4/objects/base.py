65bbfe7daa00eca7784ea5a3f0cd1bdd62e90a05
ezilber-akamai <ezilber@akamai.com>
2023-04-07 12:11:47 -0400

    Fixed domain update issue

--- linode_api4/objects/base.py
+++ linode_api4/objects/base.py
@@ -196,7 +196,7 @@ class Base(object, metaclass=FilterableMetaclass):
             elif isinstance(v,MappedObject):
                 result[k] = v.dict
 
-        return result
+        return {k: v for k, v in result.items() if v}
 
     def _api_get(self):
         """
--- test/objects/domain_test.py
+++ test/objects/domain_test.py
@@ -6,10 +6,12 @@ class DomainGeneralTest(ClientBaseCase):
     Tests methods of the Domain class.
     """
 
-    def test_reproduce_error(self):
+    def test_save_null_values_excluded(self):
         with self.mock_put('domains/12345') as m:
             domain = self.client.load(Domain, 12345)
 
             domain.type = "slave"
             domain.master_ips = ["127.0.0.1"]
             domain.save()
+
+            self.assertTrue('group' not in m.call_data.keys())
