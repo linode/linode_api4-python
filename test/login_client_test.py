98eb52bdaaac9bc805643cab114f3ba540d641cf
Lena Garber <lgarber@akamai.com>
2023-04-14 10:57:14 -0400

    Make format

--- test/login_client_test.py
+++ test/login_client_test.py
@@ -23,18 +23,33 @@ class OAuthScopesTest(TestCase):
         Tests parsing many scopes
         """
         scopes = OAuthScopes.parse("linodes:read_only domains:read_write")
-        self.assertEqual(scopes, [OAuthScopes.Linodes.read_only, OAuthScopes.Domains.read_write])
+        self.assertEqual(
+            scopes,
+            [OAuthScopes.Linodes.read_only, OAuthScopes.Domains.read_write],
+        )
 
     def test_parse_scopes_many_comma_delimited(self):
         """
         Tests parsing many scopes that are comma-delimited (which preserves old behavior)
         """
-        scopes = OAuthScopes.parse("nodebalancers:read_write,stackscripts:*,events:read_only")
-        self.assertEqual(scopes, [OAuthScopes.NodeBalancers.read_write, OAuthScopes.StackScripts.all, OAuthScopes.Events.read_only])
-        
+        scopes = OAuthScopes.parse(
+            "nodebalancers:read_write,stackscripts:*,events:read_only"
+        )
+        self.assertEqual(
+            scopes,
+            [
+                OAuthScopes.NodeBalancers.read_write,
+                OAuthScopes.StackScripts.all,
+                OAuthScopes.Events.read_only,
+            ],
+        )
+
     def test_parse_scopes_all(self):
         """
         Tests parsing * scopes
         """
         scopes = OAuthScopes.parse("*")
-        self.assertEqual(scopes, [getattr(c, "all") for c in OAuthScopes._scope_families.values()])
+        self.assertEqual(
+            scopes,
+            [getattr(c, "all") for c in OAuthScopes._scope_families.values()],
+        )
