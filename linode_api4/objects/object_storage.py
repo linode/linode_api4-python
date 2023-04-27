c02edd61973a74b183482e26a24b7d406b62692f
Ye Chen <yechen@akamai.com>
2023-04-27 11:52:11 -0400

    add api doc

--- linode_api4/objects/object_storage.py
+++ linode_api4/objects/object_storage.py
@@ -4,6 +4,8 @@ from linode_api4.objects import Base, DerivedBase, Image, Property, Region
 class ObjectStorageCluster(Base):
     """
     A cluster where Object Storage is available.
+
+    API documentation: https://www.linode.com/docs/api/object-storage/#cluster-view
     """
 
     api_endpoint = "/object-storage/clusters/{id}"
@@ -20,6 +22,8 @@ class ObjectStorageCluster(Base):
 class ObjectStorageKeys(Base):
     """
     A keypair that allows third-party applications to access Linode Object Storage.
+
+    API documentation: https://www.linode.com/docs/api/object-storage/#object-storage-key-view
     """
 
     api_endpoint = "/object-storage/keys/{id}"
