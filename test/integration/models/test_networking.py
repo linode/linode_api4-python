12c60ef517aa5c875addafa388c76755b5c2c4c6
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2023-08-15 11:44:33 -0400

    fix: `ip_addresses_share` to remove shared IPs (#316)
    ## 📝 Description
    
    Fix an issue in `ip_addresses_share` to allow it unshare IPs. 
    
    In the API doc, entering an empty IP array is allowed and it means
    removing the shared IPs from the Linode. In the previous implementation,
    we always looked for `ips[0]` when building the request param. It
    brought the index out of range error when an empty IP array is given.
    
    ## ✔️ How to Test
    
    `tox`

--- test/integration/models/test_networking.py
+++ test/integration/models/test_networking.py
@@ -13,3 +13,39 @@ def test_get_networking_rules(get_client, create_firewall):
     assert "inbound_policy" in str(rules)
     assert "outbound" in str(rules)
     assert "outbound_policy" in str(rules)
+
+
+@pytest.mark.smoke
+def test_ip_addresses_share(self):
+    """
+    Test that you can share IP addresses with Linode.
+    """
+    ip_share_url = "/networking/ips/share"
+    ips = ["127.0.0.1"]
+    linode_id = 12345
+    with self.mock_post(ip_share_url) as m:
+        result = self.client.networking.ip_addresses_share(ips, linode_id)
+
+        self.assertIsNotNone(result)
+        self.assertEqual(m.call_url, ip_share_url)
+        self.assertEqual(
+            m.call_data,
+            {
+                "ips": ips,
+                "linode": linode_id,
+            },
+        )
+
+    # Test that entering an empty IP array is allowed.
+    with self.mock_post(ip_share_url) as m:
+        result = self.client.networking.ip_addresses_share([], linode_id)
+
+        self.assertIsNotNone(result)
+        self.assertEqual(m.call_url, ip_share_url)
+        self.assertEqual(
+            m.call_data,
+            {
+                "ips": [],
+                "linode": linode_id,
+            },
+        )
