e32574542f4875f9fc90a9ed0f99b6570c95bd76
ykim-1 <ykim@akamai.com>
2023-09-15 10:30:01 -0700

    lint

--- test/script/add_to_xml_test_report.py
+++ test/script/add_to_xml_test_report.py
@@ -2,11 +2,15 @@ import argparse
 import xml.etree.ElementTree as ET
 
 # Parse command-line arguments
-parser = argparse.ArgumentParser(description='Modify XML with workflow information')
-parser.add_argument('--branch_name', required=True)
-parser.add_argument('--gha_run_id', required=True)
-parser.add_argument('--gha_run_number', required=True)
-parser.add_argument('--xmlfile', required=True)  # Added argument for XML file path
+parser = argparse.ArgumentParser(
+    description="Modify XML with workflow information"
+)
+parser.add_argument("--branch_name", required=True)
+parser.add_argument("--gha_run_id", required=True)
+parser.add_argument("--gha_run_number", required=True)
+parser.add_argument(
+    "--xmlfile", required=True
+)  # Added argument for XML file path
 
 args = parser.parse_args()
 
@@ -16,13 +20,13 @@ tree = ET.parse(xml_file_path)
 root = tree.getroot()
 
 # Create new elements for the information
-branch_name_element = ET.Element('branch_name')
+branch_name_element = ET.Element("branch_name")
 branch_name_element.text = args.branch_name
 
-gha_run_id_element = ET.Element('gha_run_id')
+gha_run_id_element = ET.Element("gha_run_id")
 gha_run_id_element.text = args.gha_run_id
 
-gha_run_number_element = ET.Element('gha_run_number')
+gha_run_number_element = ET.Element("gha_run_number")
 gha_run_number_element.text = args.gha_run_number
 
 # Add the new elements to the root of the XML
@@ -34,4 +38,4 @@ root.append(gha_run_number_element)
 modified_xml_file_path = xml_file_path  # Overwrite it
 tree.write(modified_xml_file_path)
 
-print(f'Modified XML saved to {modified_xml_file_path}')
+print(f"Modified XML saved to {modified_xml_file_path}")
--- test/script/test_report_upload_script.py
+++ test/script/test_report_upload_script.py
@@ -1,11 +1,12 @@
-import boto3
-import sys
 import os
+import sys
+
+import boto3
 from botocore.exceptions import NoCredentialsError
 
-ACCESS_KEY = os.environ.get('LINODE_CLI_OBJ_ACCESS_KEY')
-SECRET_KEY = os.environ.get('LINODE_CLI_OBJ_SECRET_KEY')
-BUCKET_NAME = 'test_reports'
+ACCESS_KEY = os.environ.get("LINODE_CLI_OBJ_ACCESS_KEY")
+SECRET_KEY = os.environ.get("LINODE_CLI_OBJ_SECRET_KEY")
+BUCKET_NAME = "test_reports"
 
 linode_obj_config = {
     "aws_access_key_id": ACCESS_KEY,
@@ -16,25 +17,27 @@ linode_obj_config = {
 
 def upload_to_linode_object_storage(file_name):
     try:
-        s3 = boto3.client('s3', **linode_obj_config)
+        s3 = boto3.client("s3", **linode_obj_config)
 
         s3.upload_file(Filename=file_name, Bucket=BUCKET_NAME, Key=file_name)
 
-        print(f'Successfully uploaded {file_name} to Linode Object Storage.')
+        print(f"Successfully uploaded {file_name} to Linode Object Storage.")
 
     except NoCredentialsError:
-        print('Credentials not available. Ensure you have set your AWS credentials.')
+        print(
+            "Credentials not available. Ensure you have set your AWS credentials."
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: python upload_to_linode.py <file_name>')
+        print("Usage: python upload_to_linode.py <file_name>")
         sys.exit(1)
 
     file_name = sys.argv[1]
 
     if not file_name:
-        print('Error: The provided file name is empty or invalid.')
+        print("Error: The provided file name is empty or invalid.")
         sys.exit(1)
 
     upload_to_linode_object_storage(file_name)
