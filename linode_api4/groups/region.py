e07c508c8f8410232e99655b405710887d0d6fed
Lena Garber <lgarber@akamai.com>
2023-11-13 17:13:06 -0500

    Avail docs

--- linode_api4/groups/region.py
+++ linode_api4/groups/region.py
@@ -26,6 +26,20 @@ class RegionGroup(Group):
         return self.client._get_and_filter(Region, *filters)
 
     def availability(self, *filters):
+        """
+        Returns the availability of Linode plans within a Region.
+
+
+        API Documentation: https://www.linode.com/docs/api/regions/#regions-availability-list
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: A list of entries describing the availability of a plan in a region.
+        :rtype: PaginatedList of RegionAvailabilityEntry
+        """
+
         return self.client._get_and_filter(
             RegionAvailabilityEntry, *filters, endpoint="/regions/availability"
         )
