ccd1a55211f3d72f586102972366b21f6576cf44
Lena Garber <lgarber@akamai.com>
2023-11-13 16:26:48 -0500

    Add filter assertion

--- test/unit/objects/region_test.py
+++ test/unit/objects/region_test.py
@@ -1,3 +1,4 @@
+import json
 from test.unit.base import ClientBaseCase
 
 from linode_api4.objects import Region, Type
@@ -29,7 +30,8 @@ class RegionTest(ClientBaseCase):
 
         with self.mock_get("/regions/availability") as m:
             avail_entries = self.client.regions.availability(
-                RegionAvailabilityEntry.filters.region == "us-east"
+                RegionAvailabilityEntry.filters.region == "us-east",
+                RegionAvailabilityEntry.filters.plan == "premium4096.7",
             )
 
             assert len(avail_entries) > 0
@@ -46,6 +48,7 @@ class RegionTest(ClientBaseCase):
             # Ensure all three pages are read
             assert m.call_count == 3
             assert m.mock.call_args_list[0].args[0] == "//regions/availability"
+
             assert (
                 m.mock.call_args_list[1].args[0]
                 == "//regions/availability?page=2&page_size=100"
@@ -54,6 +57,12 @@ class RegionTest(ClientBaseCase):
                 m.mock.call_args_list[2].args[0]
                 == "//regions/availability?page=3&page_size=100"
             )
+
+            # Ensure the filter headers are correct
+            for k, call in m.mock.call_args_list:
+                assert json.loads(call.get("headers").get("X-Filter")) == {
+                    "+and": [{"region": "us-east"}, {"plan": "premium4096.7"}]
+                }
 
     def test_region_availability(self):
         """
