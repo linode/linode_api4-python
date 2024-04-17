6943fec4375a67159d1a15d91702d2f36c796cad
Lena Garber <lgarber@akamai.com>
2024-04-17 14:43:49 -0400

    Update region unit test

--- test/unit/objects/region_test.py
+++ test/unit/objects/region_test.py
@@ -23,6 +23,12 @@ class RegionTest(ClientBaseCase):
         self.assertEqual(region.status, "ok")
         self.assertIsNotNone(region.resolvers)
         self.assertEqual(region.site_type, "core")
+        self.assertEqual(
+            region.placement_group_limits.maximum_pgs_per_customer, 5
+        )
+        self.assertEqual(
+            region.placement_group_limits.maximum_linodes_per_pg, 5
+        )
 
     def test_list_availability(self):
         """
