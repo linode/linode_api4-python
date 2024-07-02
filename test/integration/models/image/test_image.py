75c04f84d95f4fffd885942be2ff5a781cecc866
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2024-07-02 12:55:31 -0400

    new: Support Image Gen2 functionalities (#428)
    * image gen2
    
    * nit
    
    * lint
    
    * fix strenum import
    
    * sort imports
    
    * add int test
    
    * address comments
    
    * rename

--- test/integration/models/image/test_image.py
+++ test/integration/models/image/test_image.py
@@ -13,6 +13,9 @@ from linode_api4.objects import Image
 def image_upload(test_linode_client):
     label = get_test_label() + "_image"
 
+    # TODO: use get_region to get regions randomly with specific functionality
+    # region = get_region(test_linode_client, {"Functionality"})
+
     test_linode_client.image_create_upload(
         label, "us-east", "integration test image upload"
     )
@@ -42,10 +45,24 @@ def test_image_create_upload(test_linode_client):
     label = get_test_label() + "_image"
     image = test_linode_client.image_upload(
         label,
-        "us-ord",
+        "us-east",
         BytesIO(test_image_content),
         description="integration test image upload",
+        tags=["tests"],
     )
 
     assert image.label == label
     assert image.description == "integration test image upload"
+    assert image.tags[0] == "tests"
+
+
+# TODO: Image is not ready for replication yet. We'll verify this test when the API is ready.
+@pytest.mark.smoke
+def test_image_replication(test_linode_client, image_upload):
+    image = test_linode_client.load(Image, image_upload.id)
+
+    image.replicate("us-central")
+
+    assert image.label == image_upload.label
+    assert image.total_size == image_upload.size * 2
+    assert len(image.regions) == 2
