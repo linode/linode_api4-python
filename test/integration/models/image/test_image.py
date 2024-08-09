cea7eb29c994b2a5329f413707ccb78808a1504a
Ye Chen <127243817+yec-akamai@users.noreply.github.com>
2024-08-09 11:33:38 -0400

    Project: Image Services Gen2 (#445)
    * new: Support Image Gen2 functionalities (#428)
    
    * image gen2
    
    * nit
    
    * lint
    
    * fix strenum import
    
    * sort imports
    
    * add int test
    
    * address comments
    
    * rename
    
    * added LA disclamier; modified replication test case
    
    * use random region in test_linode_client with caps; use stable regions for image gen2
    
    * fix int test
    
    * fix lint
    
    * replace todo with doc link

--- test/integration/models/image/test_image.py
+++ test/integration/models/image/test_image.py
@@ -1,20 +1,24 @@
 from io import BytesIO
+from test.integration.conftest import get_region
 from test.integration.helpers import (
     delete_instance_with_test_kw,
     get_test_label,
 )
 
+import polling
 import pytest
 
 from linode_api4.objects import Image
 
 
 @pytest.fixture(scope="session")
-def image_upload(test_linode_client):
+def image_upload_url(test_linode_client):
     label = get_test_label() + "_image"
 
+    region = get_region(test_linode_client, site_type="core")
+
     test_linode_client.image_create_upload(
-        label, "us-east", "integration test image upload"
+        label, region.id, "integration test image upload"
     )
 
     image = test_linode_client.images()[0]
@@ -26,26 +30,63 @@ def image_upload(test_linode_client):
     delete_instance_with_test_kw(images)
 
 
-@pytest.mark.smoke
-def test_get_image(test_linode_client, image_upload):
-    image = test_linode_client.load(Image, image_upload.id)
-
-    assert image.label == image_upload.label
-
-
-def test_image_create_upload(test_linode_client):
+@pytest.fixture(scope="session")
+def test_uploaded_image(test_linode_client):
     test_image_content = (
         b"\x1F\x8B\x08\x08\xBD\x5C\x91\x60\x00\x03\x74\x65\x73\x74\x2E\x69"
         b"\x6D\x67\x00\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00"
     )
 
     label = get_test_label() + "_image"
+
     image = test_linode_client.image_upload(
         label,
-        "us-ord",
+        "us-east",
         BytesIO(test_image_content),
         description="integration test image upload",
+        tags=["tests"],
     )
 
-    assert image.label == label
+    yield image
+
+    image.delete()
+
+
+@pytest.mark.smoke
+def test_get_image(test_linode_client, image_upload_url):
+    image = test_linode_client.load(Image, image_upload_url.id)
+
+    assert image.label == image_upload_url.label
+
+
+def test_image_create_upload(test_linode_client, test_uploaded_image):
+    image = test_linode_client.load(Image, test_uploaded_image.id)
+
+    assert image.label == test_uploaded_image.label
     assert image.description == "integration test image upload"
+    assert image.tags[0] == "tests"
+
+
+@pytest.mark.smoke
+def test_image_replication(test_linode_client, test_uploaded_image):
+    image = test_linode_client.load(Image, test_uploaded_image.id)
+
+    # wait for image to be available for replication
+    def poll_func() -> bool:
+        image._api_get()
+        return image.status in {"available"}
+
+    try:
+        polling.poll(
+            poll_func,
+            step=10,
+            timeout=250,
+        )
+    except polling.TimeoutException:
+        print("failed to wait for image status: timeout period expired.")
+
+    # image replication works stably in these two regions
+    image.replicate(["us-east", "eu-west"])
+
+    assert image.label == test_uploaded_image.label
+    assert len(image.regions) == 2
