7786eb6279ea9f0ad7537e8e3bef1d71be902819
Lena Garber <lgarber@akamai.com>
2024-10-15 13:31:23 -0400

    Remove comma from multiline string

--- test/integration/linode_client/test_errors.py
+++ test/integration/linode_client/test_errors.py
@@ -24,5 +24,5 @@ def test_error_400(test_linode_client):
 
     assert str(api_exc) == (
         "POST /v4beta/linode/instances: [400] type: A valid plan type by that ID was not found; "
-        "region: region is not valid",
+        "region: region is not valid"
     )
