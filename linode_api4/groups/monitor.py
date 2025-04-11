40b0057e657cbac329af193bbadd3182f1f36f0d
Priya Majali <pmajali@akamai.com>
2025-04-11 11:49:04 +0530

    adding monitor APIs

---
+++ linode_api4/groups/monitor.py
@@ -0,0 +1,182 @@
+from linode_api4.errors import UnexpectedResponseError
+from linode_api4.groups import Group
+from linode_api4.objects import (
+    CreateToken,
+    DashboardByService,
+    ServiceDetails,
+    Dashboard,
+    MonitorServiceSupported,
+    MetricDefinition,
+    DashboardsByID,
+)
+
+class MonitorGroup(Group):
+    """
+    Encapsulates LKE-related methods of the :any:`LinodeClient`.  This
+    should not be instantiated on its own, but should instead be used through
+    an instance of :any:`LinodeClient`::
+
+       client = LinodeClient(token)
+       instances = client.lke.clusters() # use the LKEGroup
+
+    This group contains all features beneath the `/lke` group in the API v4.
+    """
+
+    def dashboards(self, *filters):
+        """
+        Returns a list of dashboards on your account. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/get-dashboards-all
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: A list of Dashboards.
+        :rtype: PaginatedList of Dashboard
+        """
+        return self.client._get_and_filter(Dashboard, *filters)
+
+    def dashboard_by_ID(self, dashboard_id: int, *filters):
+        """
+        Returns a dashboards on your account based on the ID passed. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/get-dashboards-by-id
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: A Dashboards.
+        :rtype: PaginatedList of the Dashboard
+        """
+        result = self.client.get(f"/monitor/dashboards/{dashboard_id}")
+
+        if not "id" in result:
+            raise UnexpectedResponseError(
+                "Unexpected response when getting Dashboard!", json=result
+            )
+        return DashboardsByID(self.client, result["id"], result)
+
+    
+    def dashboard_by_service(self, service_type: str,*filters):
+        """
+        Returns a dashboards on your account based on the service passed. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/get-dashboards
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: A Dashboards filtered by Service Type.
+        :rtype: PaginatedList of the Dashboards
+        """
+        
+        return self.client._get_and_filter(
+            DashboardByService,
+            *filters,
+            endpoint=f"/monitor/services/{service_type}/dashboards",
+        )
+
+
+    def supported_services(self, *filters):
+        """
+        Returns a list of services supported by ACLP. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/get-monitor-services
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: A list of Supported Services
+        :rtype: PaginatedList of the Dashboards
+        """
+
+        return self.client._get_and_filter(MonitorServiceSupported, *filters)
+
+    def details_by_service(self, service_type: str,*filters):
+        """
+        Returns a details about a particular service. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/get-monitor-services-for-service-type
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: Details about a  Supported Services
+        :rtype: PaginatedList of the Service 
+        """
+        return self.client._get_and_filter(
+            ServiceDetails,
+            *filters,
+            endpoint=f"/monitor/services/{service_type}",
+        )
+    
+    def metric_definitions(self, service_type: str,*filters):
+        """
+        Returns metrics for a specific service type. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/get-monitor-information
+
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: Returns a  List of metrics for a service
+        :rtype: PaginatedList of metrics 
+        """
+        return self.client._get_and_filter(
+            MetricDefinition,
+            *filters,
+            endpoint=f"/monitor/services/{service_type}/metric-definitions",
+        )
+    
+    def create_token(self, service_type: str, entity_ids: list, *filters):
+        """
+        Returns a JWE Token for a specific service type. 
+
+        .. note:: This endpoint is in beta. This will only function if base_url is set to `https://api.linode.com/v4beta`.
+        
+        API Documentation: https://techdocs.akamai.com/linode-api/reference/post-get-token
+        :param filters: Any number of filters to apply to this query.
+                        See :doc:`Filtering Collections</linode_api4/objects/filtering>`
+                        for more details on filtering.
+
+        :returns: Returns a token for a service
+        :rtype: str
+        """
+
+        params = {"entity_ids": entity_ids}
+
+        result = self.client.post(f"/monitor/services/{service_type}/token", data=params)
+
+        if "token" not in result:
+            raise UnexpectedResponseError(
+                "Unexpected response when creating token!", json=result
+            )
+        return CreateToken(self.client, result["token"], result)
+    
+   
+      
+    
+    
+
+
+
+    
+   
---
+++ test/integration/models/monitor/test_monitor.py
@@ -0,0 +1,122 @@
+import json
+
+from linode_api4.objects import (
+    MonitorServiceSupported,
+    Dashboard,
+    DashboardsByID,
+    DashboardByService,
+    ServiceDetails,
+    MetricDefinition,
+    CreateToken,
+)
+from linode_api4 import LinodeClient,MySQLDatabase
+import re
+import time
+from test.integration.helpers import (
+    get_test_label,
+    send_request_when_resource_available,
+    wait_for_condition,
+)
+
+import pytest
+
+
+
+# List all dashboards
+def test_get_all_dashboards(test_linode_client):
+    client = test_linode_client
+    dashboards = client.monitor.dashboards()
+    assert isinstance(dashboards[0],Dashboard)
+
+    dashboard_get = dashboards[0]
+    dashboard_id = dashboard_get.id
+    get_service_type = dashboard_get.service_type
+    print(f"printing svc {get_service_type}")
+
+    #Fetch Dashboard by ID
+    dashboard_by_id = client.monitor.dashboard_by_ID(dashboard_id=dashboard_id)
+    assert isinstance(dashboard_by_id, DashboardsByID)
+    assert dashboard_by_id.id == dashboard_id
+
+    #Fetch Dashboard by service_type
+    dashboards_by_svc = client.monitor.dashboard_by_service(service_type=get_service_type)
+    assert isinstance(dashboards_by_svc[0], DashboardByService)
+    assert dashboards_by_svc[0].service_type == get_service_type
+
+# List supported services   
+def test_get_supported_services(test_linode_client):
+    client = test_linode_client
+    supported_services = client.monitor.supported_services()
+    assert isinstance(supported_services[0], MonitorServiceSupported)
+
+    get_supported_service = supported_services[0].service_type
+
+    #Get details for a particular service
+    service_details = client.monitor.details_by_service(service_type=get_supported_service)
+    assert isinstance(service_details[0],ServiceDetails)
+    assert service_details[0].service_type == get_supported_service
+
+    #Get Metric definition details for that particular service
+    metric_definitions = client.monitor.metric_definitions(service_type=get_supported_service)
+    assert isinstance(metric_definitions[0],MetricDefinition)
+
+# Test Helpers
+def get_db_engine_id(client: LinodeClient, engine: str):
+    engines = client.database.engines()
+    engine_id = ""
+    for e in engines:
+        if e.engine == engine:
+            engine_id = e.id
+
+    return str(engine_id)
+
+@pytest.fixture(scope="session")
+def test_create_and_test_db(test_linode_client):
+    client = test_linode_client
+    label = get_test_label() + "-sqldb"
+    region = "us-ord"
+    engine_id = get_db_engine_id(client, "mysql")
+    dbtype = "g6-standard-1"
+
+
+    db = client.database.mysql_create(
+        label=label,
+        region=region,
+        engine=engine_id,
+        ltype=dbtype,
+        cluster_size=None,
+    )
+
+    def get_db_status():
+        return db.status == "active"
+
+    # TAKES 15-30 MINUTES TO FULLY PROVISION DB
+    wait_for_condition(60, 2000, get_db_status)
+
+    yield db
+    send_request_when_resource_available(300, db.delete)
+
+def test_my_db_functionality(test_linode_client, test_create_and_test_db):
+    client = test_linode_client
+    assert test_create_and_test_db.status == "active"
+    
+    entity_id = test_create_and_test_db.id
+
+
+    # create token for the particular service
+    token = client.monitor.get_token(service_type="dbaas", entity_ids=[entity_id])
+    assert isinstance(token, CreateToken)
+    assert len(token.token) > 0, "Token should not be empty"
+    assert hasattr(token, "token"), "Response object has no 'token' attribute"
+
+
+    
+  
+   
+
+
+
+
+
+
+
