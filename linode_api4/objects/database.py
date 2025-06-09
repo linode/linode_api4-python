23510bbd5a856eadcfac93877adbb31cb447f63c
Erik Zilber <ezilber@akamai.com>
2025-06-09 15:26:42 -0400

    Added support for VPC DBaaS Integration (#560)
    * Implemented support for VPC DBaaS Integration
    
    * Added unit tests

--- linode_api4/objects/database.py
+++ linode_api4/objects/database.py
@@ -74,6 +74,18 @@ class DatabaseEngine(Base):
         Base.invalidate(self)
 
 
+@dataclass
+class DatabasePrivateNetwork(JSONObject):
+    """
+    DatabasePrivateNetwork is used to specify
+    a Database Cluster's private network settings during its creation.
+    """
+
+    vpc_id: Optional[int] = None
+    subnet_id: Optional[int] = None
+    public_access: Optional[bool] = None
+
+
 @deprecated(
     reason="Backups are not supported for non-legacy database clusters."
 )
@@ -304,6 +316,9 @@ class MySQLDatabase(Base):
         "engine_config": Property(
             mutable=True, json_object=MySQLDatabaseConfigOptions
         ),
+        "private_network": Property(
+            mutable=True, json_object=DatabasePrivateNetwork, nullable=True
+        ),
     }
 
     @property
@@ -469,6 +484,9 @@ class PostgreSQLDatabase(Base):
         "version": Property(),
         "engine_config": Property(
             mutable=True, json_object=PostgreSQLDatabaseConfigOptions
+        ),
+        "private_network": Property(
+            mutable=True, json_object=DatabasePrivateNetwork, nullable=True
         ),
     }
 
@@ -636,6 +654,9 @@ class Database(Base):
         "updated": Property(),
         "updates": Property(),
         "version": Property(),
+        "private_network": Property(
+            json_object=DatabasePrivateNetwork, nullable=True
+        ),
     }
 
     @property
