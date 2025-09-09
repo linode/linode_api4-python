a5e81ccc46f0e26c107f173df4322de2cde6d758
Srinidhi Bhat <srbha@akamai.com>
2025-09-09 21:09:49 +0530

    DI-26927 Corrected json to object modifications issues

--- linode_api4/objects/monitor.py
+++ linode_api4/objects/monitor.py
@@ -344,24 +344,16 @@ class AlertDefinition(Base):
     def __init__(self, client, id, json=None):
         super().__init__(client, id, json)
 
-    # @property
-    # def type(self):
-    #     return self._type
-
-    # @type.setter
-    # def type(self, value):
-    #     self._type = value
-
     def _populate(self, json):
         """
         Populates this object with data from a JSON dictionary.
         """
         for key, value in json.items():
-            # Map "class" to "_class" because "class" is a reserved word in Python.
-            if key == "class":
-                setattr(self, "_class", value)
-            else:
-                setattr(self, key, value)
+            #class and type are reserved words in Python.
+            reserved = {"class": "_class", "type": "_type"}
+            for json_key, json_value in json.items():
+                attr = reserved.get(json_key, json_key)
+                setattr(self, attr, json_value)
 
 
 @dataclass
