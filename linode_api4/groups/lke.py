966859c71bf7b66137a9570205d91f57a4c78aff
rammanoj <rammanojpotla1608@gmail.com>
2026-01-13 09:23:29 -0500

    address feedback

--- linode_api4/groups/lke.py
+++ linode_api4/groups/lke.py
@@ -122,7 +122,9 @@ class LKEGroup(Group):
         if node_pools is None:
             node_pools = []
 
-        if len(node_pools) == 0 and (tier is None or tier.lower() != "enterprise"):
+        if len(node_pools) == 0 and (
+            tier is None or tier.lower() != "enterprise"
+        ):
             raise ValueError(
                 "LKE standard clusters must have at least one node pool."
             )
