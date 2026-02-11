fe92e9a1e93a2fa1b9842098f6683772a958d79d
Michal Wojcik <miwojci@akamai.com>
2026-02-11 08:46:27 +0100

    python-sdk: Add region capability enum

--- linode_api4/objects/region.py
+++ linode_api4/objects/region.py
@@ -3,6 +3,66 @@ from typing import List, Optional
 
 from linode_api4.errors import UnexpectedResponseError
 from linode_api4.objects.base import Base, JSONObject, Property
+from linode_api4.objects.serializable import StrEnum
+
+
+class Capability(StrEnum):
+    """
+    An enum class representing the capabilities that Linode offers
+    across different regions and services.
+
+    These capabilities indicate what services are available in each data center.
+    """
+
+    ACLB = "Akamai Cloud Load Balancer"
+    ACLP = "Akamai Cloud Pulse"
+    ACLPStreams = "Akamai Cloud Pulse Streams"
+    AkamaiRAMProtection = "Akamai RAM Protection"
+    Backups = "Backups"
+    BareMetal = "Bare Metal"
+    BlockStorage = "Block Storage"
+    BlockStorageEncryption = "Block Storage Encryption"
+    BlockStorageMigrations = "Block Storage Migrations"
+    BlockStoragePerformanceB1 = "Block Storage Performance B1"
+    BlockStoragePerformanceB1Default = "Block Storage Performance B1 Default"
+    CloudFirewall = "Cloud Firewall"
+    CloudFirewallRuleSet = "Cloud Firewall Rule Set"
+    CloudNAT = "Cloud NAT"
+    DBAAS = "Managed Databases"
+    DBAASBeta = "Managed Databases Beta"
+    DiskEncryption = "Disk Encryption"
+    DistributedPlans = "Distributed Plans"
+    EdgePlans = "Edge Plans"
+    GPU = "GPU Linodes"
+    KubernetesEnterprise = "Kubernetes Enterprise"
+    KubernetesEnterpriseBYOVPC = "Kubernetes Enterprise BYO VPC"
+    KubernetesEnterpriseDualStack = "Kubernetes Enterprise Dual Stack"
+    LADiskEncryption = "LA Disk Encryption"
+    LinodeInterfaces = "Linode Interfaces"
+    Linodes = "Linodes"
+    LKE = "Kubernetes"
+    LKEControlPlaneACL = "LKE Network Access Control List (IP ACL)"
+    LKEHAControlPlanes = "LKE HA Control Planes"
+    MachineImages = "Machine Images"
+    MaintenancePolicy = "Maintenance Policy"
+    Metadata = "Metadata"
+    NLB = "Network LoadBalancer"
+    NodeBalancers = "NodeBalancers"
+    ObjectStorage = "Object Storage"
+    ObjectStorageAccessKeyRegions = "Object Storage Access Key Regions"
+    ObjectStorageEndpointTypes = "Object Storage Endpoint Types"
+    PlacementGroup = "Placement Group"
+    PremiumPlans = "Premium Plans"
+    QuadraT1UVPU = "NETINT Quadra T1U"
+    SMTPEnabled = "SMTP Enabled"
+    StackScripts = "StackScripts"
+    SupportTicketSeverity = "Support Ticket Severity"
+    Vlans = "Vlans"
+    VPCs = "VPCs"
+    VPCDualStack = "VPC Dual Stack"
+    VPCIPv6Stack = "VPC IPv6 Stack"
+    VPCIPv6LargePrefixes = "VPC IPv6 Large Prefixes"
+    VPCsExtra = "VPCs Extra"
 
 
 @dataclass
