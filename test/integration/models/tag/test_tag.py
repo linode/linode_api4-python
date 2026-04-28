f058877bc3756997b8b111bb0a284e65ab53ec4b
Maciej Wilk <mawilk@akamai.com>
2026-04-28 14:12:37 +0200

    Create int tests for Reserved IPs: tags #2

--- test/integration/models/tag/test_tag.py
+++ test/integration/models/tag/test_tag.py
@@ -2,7 +2,7 @@ from test.integration.helpers import get_test_label
 
 import pytest
 
-from linode_api4.objects import Tag
+from linode_api4.objects import ReservedIPAddress, Tag
 
 
 @pytest.fixture
@@ -15,6 +15,19 @@ def test_tag(test_linode_client):
     tag.delete()
 
 
+@pytest.fixture
+def create_tag_with_reserved_ip(test_linode_client, create_reserved_ip):
+    unique_tag = get_test_label() + "_tag"
+    reserved_ip = create_reserved_ip
+
+    tag = test_linode_client.tags.create(unique_tag, reserved_ipv4_addresses=[reserved_ip.address])
+    reserved_ip = test_linode_client.networking.reserved_ips(ReservedIPAddress.address==reserved_ip.address)[0]
+
+    yield tag, reserved_ip
+
+    tag.delete()
+
+
 @pytest.mark.smoke
 def test_get_tag(test_linode_client, test_tag):
     tag = test_linode_client.load(Tag, test_tag.id)
@@ -22,14 +35,15 @@ def test_get_tag(test_linode_client, test_tag):
     assert tag.id == test_tag.id
 
 
-@pytest.mark.skip(reason="This test is currently blocked - API does not support tagging reserved IPs yet")
-def test_get_tag_with_reserved_ip(test_linode_client, create_reserved_ip):
-    unique_tag = get_test_label() + "_tag"
-    reserved_ip = create_reserved_ip
+def test_get_tag_with_reserved_ip(test_linode_client, create_tag_with_reserved_ip):
+    tag, reserved_ip = create_tag_with_reserved_ip
+    tag = test_linode_client.load(Tag, tag.id).objects[0]
 
-    tag = test_linode_client.tags.create(unique_tag, reserved_ipv4_addresses=[reserved_ip.address])
-    tag = test_linode_client.load(Tag, tag.id)
-    assert tag.type == "reserved_ipv4_address"
-    # assert tag.data...
+    assert vars(tag).keys() == vars(reserved_ip).keys()
+    assert tag.address == reserved_ip.address
+    assert tag.reserved == reserved_ip.reserved
+    assert tag.tags == reserved_ip.tags
 
     tag.delete()
+    reserved_ip = test_linode_client.networking.reserved_ips(ReservedIPAddress.address==reserved_ip.address)
+    assert len(reserved_ip) == 0
