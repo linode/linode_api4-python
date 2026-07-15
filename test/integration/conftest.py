bfc2e595f3233fbf8349f6ca752b6fa1dce9a9ce
Maciej Wilk <mawilk@akamai.com>
2026-07-15 09:43:01 +0200

    Change rdma fixtures scope

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -484,7 +484,7 @@ def create_vpc(test_linode_client):
     vpc.delete()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def create_vpc_with_rdma_type(test_linode_client):
     client = test_linode_client
     label = get_test_label(length=10)
@@ -540,12 +540,12 @@ def create_vpc_with_subnet_and_linode(
     instance.delete()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def create_vpc_with_subnet_and_rdma_type(create_vpc_with_rdma_type):
     vpc_rdma = create_vpc_with_rdma_type
     label = get_test_label(length=10)
 
-    subnet_rdma = create_vpc_with_rdma_type.subnet_create(
+    subnet_rdma = vpc_rdma.subnet_create(
         label=label,
         ipv4="10.0.0.0/24",
     )
